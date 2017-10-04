3793db8cc0edc0339c3d090c52722dd592e721c0
Julien Levasseur <dko2327@lab-vfbl5831-0.mvt.desjardins.dev>
2017-10-04 14:25:03 -0400

    correct main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,4 @@
-# Doc : https://www.terraform.io/docs/providers/openstack/index.html
-
-provider "openstack" {
-  user_name   = "${var.username}"
-  tenant_name = "${var.tenant_name}"
-  password    = "${var.password}"
-  auth_url    = "${var.auth_url}"
-  region      = "${var.region}"
+provider "consul" {
+  datacenter = "${var.datacenter}"
+  path = "${var.path}"
 }
